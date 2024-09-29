cc87fadf3cf66869517d9ffdd23bc72fcba42247
Sakhile Mamba <sakhymamba@gmail.com>
2024-09-29 03:35:22 +0200

    used double quotes

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,17 @@
-import ProductData from './ProductData.mjs';
-import ProductList from './ProductList.mjs';
-import ProductDetails from './ProductDetails.mjs';
-import { getParam } from './utils.mjs';  
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import ProductDetails from "./ProductDetails.mjs";
+import { getParam } from "./utils.mjs";
 
-const productId = getParam('product');
+const productId = getParam("product");
 
 if (productId) {
-  const dataSource = new ProductData('tents');
+  const dataSource = new ProductData("tents");
   const product = new ProductDetails(productId, dataSource);
   product.init();
 } else {
-  const dataSource = new ProductData('tents');
-  const element = document.querySelector('.product-list');
-  const listing = new ProductList('Tents', dataSource, element);
+  const dataSource = new ProductData("tents");
+  const element = document.querySelector(".product-list");
+  const listing = new ProductList("Tents", dataSource, element);
   listing.init();
 }
